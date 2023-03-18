52b761a3451f46c7e417982ab1d613244e47590a
hibissscus <hibissscus@gmail.com>
2023-03-18 19:13:29 +0100

    - BuildAgentTest

--- src/test/kotlin/teamcity/rest/build/BuildAgentPoolTest.kt
+++ src/test/kotlin/teamcity/rest/build/BuildAgentPoolTest.kt
@@ -2,17 +2,31 @@ package teamcity.rest.build
 
 import org.testng.annotations.Test
 import teamcity.rest.TestBase
-import teamcity.rest.callPublicPropertiesAndFetchMethods
-import teamcity.rest.publicInstance
-
+import teamcity.rest.testBuildAgentPool
+import teamcity.rest.testProject
+import kotlin.test.assertTrue
 
 @Test(groups = ["build"])
 class BuildAgentPoolTest : TestBase() {
 
     @Test
-    fun `test all`() {
-        publicInstance().buildAgentPools().all().forEach {
-            callPublicPropertiesAndFetchMethods(it)
-        }
+    fun `test no registered build agents`() {
+        assertTrue(teamCityInstance.buildAgents().all().none())
+    }
+
+    @Test
+    fun `check agent pool has some build agents`() {
+        assertTrue(teamCityInstance.buildAgentPools().all().any())
+    }
+
+    @Test
+    fun `check agent pools has specific one`() {
+        assertTrue(teamCityInstance.buildAgentPools().all().any { it.name == testBuildAgentPool })
+    }
+
+    @Test
+    fun `check agent pool assigned to the project`() {
+        assertTrue(teamCityInstance.buildAgentPools().all()
+            .any { it -> it.name == "TestBuildAgentPool" && it.projects.any { it.name == testProject.name } })
     }
 }
