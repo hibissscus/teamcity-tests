c5500b312bd86244046c4b30783d863363f6dbe9
hibissscus <hibissscus@gmail.com>
2023-03-18 19:31:28 +0100

    - QueueTest

--- src/test/kotlin/teamcity/rest/build/QueueTest.kt
+++ src/test/kotlin/teamcity/rest/build/QueueTest.kt
@@ -1,7 +1,8 @@
 package teamcity.rest.build
 
-import org.jetbrains.teamcity.rest.ProjectId
+import org.jetbrains.teamcity.rest.BuildState
 import org.testng.annotations.Test
+import teamcity.rest.testProject
 import kotlin.test.assertEquals
 
 @Test(groups = ["build"])
@@ -10,16 +11,18 @@ class QueueTest : BuildTestBase() {
     @Test
     fun `test new build gets into queue`() {
         val defaultBuildRun = defaultBuildRun()
+        awaitState(defaultBuildRun.id, BuildState.QUEUED, 30000L)
         teamCityInstance.buildQueue().queuedBuilds().forEach {
             assertEquals(it, defaultBuildRun)
         }
     }
 
     @Test
-    fun `test project queue all properties thus evaluating them`() {
-        teamCityInstance.buildQueue().queuedBuilds(ProjectId("Test Project")).forEach {
-            it.toString()
-            println(it)
+    fun `test specific project gets into queue`() {
+        val defaultBuildRun = defaultBuildRun()
+        awaitState(defaultBuildRun.id, BuildState.QUEUED, 30000L)
+        teamCityInstance.buildQueue().queuedBuilds(testProject.id).forEach {
+            assertEquals(it, defaultBuildRun)
         }
     }
 }
