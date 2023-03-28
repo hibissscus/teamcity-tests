bf4e3e114c1e4a456d8ac011c8d5a69c3102d63f
hibissscus <hibissscus@gmail.com>
2023-03-28 21:28:52 +0200

    - Selenide configuration.timeout

--- src/test/kotlin/teamcity/ui/test/UITestBase.kt
+++ src/test/kotlin/teamcity/ui/test/UITestBase.kt
@@ -1,6 +1,7 @@
 package teamcity.ui.test
 
 import com.codeborne.selenide.Configuration
+import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.logevents.SelenideLogger
 import io.qameta.allure.selenide.AllureSelenide
 import org.testng.annotations.BeforeClass
@@ -23,5 +24,6 @@ open class UITestBase {
         if (!Strings.isNullOrEmpty(optHeadless)) Configuration.headless = optHeadless.toBoolean()
         println("baseUrl:" + Configuration.baseUrl + " remote:" + Configuration.remote + " headless:" + Configuration.headless)
         SelenideLogger.addListener("allure", AllureSelenide())
+        Configuration.timeout = 10000
     }
 }
